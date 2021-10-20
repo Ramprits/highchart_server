45a9fbb2ad7e823f6f50d3024c5b55da62cf5373
Ramprit Sahani <palak.ghai@torinit.ca>
2021-10-20 16:33:40 +0530

    Testing application

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const chartExporter = require("highcharts-export-server");
@@ -8,35 +8,67 @@ const url = require("url");
 chartExporter.initPool();
 
 /* GET users listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  let fileUrl;
 
-    let fileUrl;
+  // Export chart using these options
+  chartExporter.export(
+    {
+      type: "png",
+      options: req.body.options,
+      width: 1200,
+    },
+    async (err, res) => {
+      let imageb64 = res.data;
+      if (res.data) {
+        fileUrl = await imageUpload(imageb64);
+        console.log(fileUrl);
+      }
+      // Filename of the output. In this case, we will write the image
+      // to the same directory of the initialization script.
+      // let outputFile = "./output-chart.png";
 
-// Export chart using these options
-    chartExporter.export({
-        type: "png",
-        options: req.body.options,
-        width: 1200
-    }, async (err, res) => {
-        let imageb64 = res.data;
-        if (res.data) {
-            fileUrl = await imageUpload(imageb64)
-            console.log(fileUrl)
-        }
-        // Filename of the output. In this case, we will write the image
-        // to the same directory of the initialization script.
-        // let outputFile = "./output-chart.png";
+      // Save the image data to a file
+      // fs.writeFileSync(outputFile, imageb64, "base64", function (err) {
+      //     if (err) console.log(err);
+      // });
+      chartExporter.killPool();
+    }
+  );
+  setTimeout(() => {
+    res.json({ message: "Successfully generated chart", fileUrl });
+  }, 3000);
+});
 
-        // Save the image data to a file
-        // fs.writeFileSync(outputFile, imageb64, "base64", function (err) {
-        //     if (err) console.log(err);
-        // });
-        chartExporter.killPool();
-    });
-    setTimeout(() => {
-        res.json({message: "Successfully generated chart", fileUrl});
-    }, 10000)
+router.post("/test", async (req, res) => {
+  let fileUrl;
 
+  // Export chart using these options
+  chartExporter.export(
+    {
+      type: "png",
+      options: req.body.options,
+      width: 1200,
+    },
+    async (err, res) => {
+      let imageb64 = res.data;
+      if (res.data) {
+        fileUrl = await imageUpload(imageb64);
+        console.log(fileUrl);
+      }
+      // Filename of the output. In this case, we will write the image
+      // to the same directory of the initialization script.
+      // let outputFile = "./output-chart.png";
+
+      // Save the image data to a file
+      // fs.writeFileSync(outputFile, imageb64, "base64", function (err) {
+      //     if (err) console.log(err);
+      // });
+      chartExporter.killPool();
+    }
+  );
+
+  res.json({ message: "Successfully generated chart", fileUrl });
 });
 
 module.exports = router;
